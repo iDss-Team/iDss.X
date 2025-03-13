7af719611b8c396a4c81e7da4e08ead74701dec8
Ari Firmansyah <arifrmnsyh@gmail.com>
2025-03-13 15:48:47 +0700

    L03 - update logo iDss

---
+++ Models/Authentication.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iDss.X.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int branchid { get; set; }
+        public string grouptype { get; set; }
+        public DateTime? passexpireddate { get; set; }
+        public bool passexpired { get; set; } = false;
+        public int loginattempt { get; set; }
+        public string securitycode { get; set; }
+    }
+}
